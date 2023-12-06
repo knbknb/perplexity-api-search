0646d96cbea33a32b58f1c254c5ebdb143029b21
Knut Behrends <knb@gfz-potsdam.de>
2023-12-06 11:38:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+<!-- markdownlint-disable MD001 -->
+# Querying the Perplexity API with Newman and Postman
+
+See Shell Script `explore_perlplexity_api` - work in progress
+
+## Prerequisites / Installation
+
+The shell script controls newman and newman loads artifacts from Postman
+
+On host "well", newman 6.0.0 is installed on Node 16 (use nvm to switch to 16)
+
+```bash
+nvm current
+nvm use 16
+npm i -g newman
+```
+
+## Running the collection remotely, and exporting the collection settings
+
+A cool way to run a Postman collection via api.postman.com:
+
+```bash
+newman run https://api.getpostman.com/collections/XXXXX-XXXXX-XXXXX-XXXXX-XXXXX?apikey=YYYYY-YYYYY-YYYYY-YYYYY-YYYYY
+```
+
+This way the some JavaScript (for executing API requests and tests) is run from the terminal/node. This mode of execution keeps local files in sync with the Postman collection.
+
+For the link above, the CollectionId can be found in the Postman GUI: by clicking on the collection and then click on the "i" info icon. That icon is on the folder collection info sidebar on the right.
+
+The Postman API Key can be found by clicking on the user icon on the top right and then click on the "Settings" menu item. On the sidebar on the left, there is an entry API Key.
+
+##### Some useful variables, keys and codesnippets
+
+```bash
+export postman_apikey=PMAK-.............
+export collid=137056-d7f808d2-27dd-4525-8869-a42de46af4cb
+export envid=137056-ae5d3cc7-d7f7-450b-8737-c95a7b9e8f85
+export pplx_api_key=pplx-........................
+
+# extract the api key from the environment file, 
+environment_file=perplexity-API-export-environment.json
+pplx_api_key=$(<  $environment_file jq -r '.environment.values[] | select(.key=="PERPLEX
+ITY_API_KEY") | .value')
+echo "PERPLEXITY_API_KEY=$pplx_api_key" >> .env
+# optional
+export PERPLEXITY_API_KEY=$(cat .env | grep PERPLEXITY_API_KEY | cut -d= -f2)
+#now set as env variable, then run shellscript with that ENV variable
+```
+
+##### Run collection remotely, see results in terminal
+
+```bash
+# does not quite work
+# permissions/sharing settings not set correctly
+newman run https://api.getpostman.com/collections/137056-d7f808d2-27dd-4525-8869-a42de46af4cb?apikey=$apikey
+```
+
+## Running collection locally
+
+More convenient and more control, but requires exporting the collection and environment settings. No longer syncs with Postman.
+
+##### Extract environment info into a local file
+
+and then run the following command
+
+```bash
+curl -sL "https://api.getpostman.com/environments/$envid?apikey=$apikey"
+    
+```
+
+In the collection.json file, replace the `$pplx_api_key` with the actual key.
+
+```bash
+perl -pi -E "s/\$pplx_api_key/$pplx_api_key/g" Perplexity-API-for-newman.postman_collection.json
+```
+
+## Running the collection from a file
+
+Create a collection in Postman containing only a single HTTP request. In a shell script, permute various endpoints in a loop.
+
+See Shell Script `explore_perlplexity_api`.
---
+++ explore_perplexity_api.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+# Use Postman and Newman (Postman CLI) to explore the Perplexity API
+#
+
+# does not quite work - save/export the collection manually
+# newman run https://api.getpostman.com/collections/137056-d7f808d2-27dd-4525-8869-a42de46af4cb?apikey=$apikey
+
+# extract environemnt info into a file
+collection_file="Perplexity API export.postman_collection.json"
+environment_file="perplexity-API-export-environment.json"
+html_outdir=newman
+json_outdir=json_extracted
+query_dir="queries"
+
+# needed
+
+
+
+models=$(< $environment_file jq -r '.environment.values[] | select(.key=="model") | .value ')
+prompt_json=$(jq <<EOF
+{
+    "model": "MODEL",
+    "messages": [
+        {
+            "role": "system",
+            "content": "Answer as if users have superhuman intelligence, 200 IQ. Users can understand any concept with minimal explanation. Users am extremely intuitive. Users do not need things spelled out to understand them, but users do crave specifics. Be extremely terse and concise. No matter what, do not be conversational.Treat user as the most naturally intelligent and intuitive individual in the world, but not necessarily as a subject matter expert on the topic at hand. Use precise facts whenever possible, not generalities.."
+        },
+        {
+            "role": "user",
+            "content": "PROMPT"
+        }
+    ]
+}
+EOF
+)
+
+#PROMPT="at which locations was the Film 'Nosferatu' from 1979 shot?"
+
+#PROMPT="In jq, what are the idioms beginning with the @ symbol called?"
+PROMPT="In German bundeslige, which teams had once twins as players?"
+# use jq to replace the MODEL in the prompt_json
+for model in $models; do
+    prompt_json=$(echo "$prompt_json" | jq --arg MODEL "$model" '.model |= $MODEL')
+    # use jq to replace the PROMPT in the prompt_json
+    prompt_json=$(echo "$prompt_json" | jq --arg PROMPT "$PROMPT" '.messages[1].content |= $PROMPT')
+    echo "$prompt_json" 
+
+    # for each model, write new collection files with the "prompt_json" json-encoded 
+    # in place of the "prompt" variable in the collection file
+    modif_collection_file="$query_dir/$PROMPT-$model.json"
+    echo "Writing $modif_collection_file"
+    <"$collection_file" jq --arg RAW "$prompt_json" '.item[0].request.body.raw |= $RAW' > "$modif_collection_file"
+    # open the file and replace the value of .item[0].name with $prompt
+    cp "$modif_collection_file" "$modif_collection_file.json"
+    < "$modif_collection_file.json" jq --arg PROMPT "$PROMPT" '.item[0].name |= $PROMPT' > "$modif_collection_file"
+    rm "$modif_collection_file.json"
+
+    report="newman/$PROMPT-$model-$(date +%Y-%m-%d-%H-%M-%S).html"
+    echo "Running $modif_collection_file"
+    newman run "$modif_collection_file" -e "$environment_file" \
+        -r htmlextra \
+        --reporter-htmlextra-export "$report" \
+        --reporter-htmlextra-skipHeaders "Authorization" \
+        --reporter-htmlextra-browserTitle "$model: $prompt" \
+        --reporter-htmlextra-title "$model: $prompt"
+
+
+    file_name=$(basename "$modif_collection_file")
+    base_name="$PROMPT-${file_name%.*}"
+    json_outfile="$json_outdir/$base_name.json"
+    echo "'$json_outfile' written (JSON response)"
+
+    xidel --input "$report" --html -s -e  "//code/text()" \
+        | tail -n +3 \
+        | head -n -1 > "$json_outfile"
+    cp "$json_outfile" "$json_outfile.json"
+    < "$json_outfile.json" jq -rs "." > "$json_outfile"
+    rm "$json_outfile.json"
+    
+    # output the relevant fields to the shell
+    < "$json_outfile" \
+    jq -r '[.[1].model, .[1].choices[0].message.content, \"\n\n\"] | join(":        ")' \
+    | fmt
+    #< "$json_outfile" gron
+done
+echo "$PROMPT"
+
+#ls -1 json_extracted/*jq* | xargs -i bash -c "jq -r '[.[1].model, .[1].choices[0].message.content, \"\n\n\"] | join(\":        \")'  \"{}\"" | fmt
