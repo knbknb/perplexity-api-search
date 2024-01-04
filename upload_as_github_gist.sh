5da2c8a3691db74d67d8f7f802f7557cd8744445
Knut Behrends <knb@gfz-potsdam.de>
2024-01-04 13:50:39 +0100

    minor imprvmtsscript upload_as_github_gist.sh

--- upload_as_github_gist.sh
+++ upload_as_github_gist.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
-
-# write a program to upload a file to github gist
+# knb 20240101
+# Shellscript to upload a bunch of textfiles to gist.github.com
 # - files are txt files in subdirectory json_all
 # - use curl to upload the file
-# - use an enironment variable for the github token
+# - use an environment variable for the github token, must be set on command line first
 # - as gist filename use the filename of the .txt file
 # - as description use line containing "content:" of each respective .txt file
-
 # - use a for loop to iterate over all files
 
 # if ENV variable is not set, exit with error
 : "${GITHUB_TOKEN:?Need to set/export env var GITHUB_TOKEN non-empty. Check .git/config for github token.}"
 
 # get list of all github gists, and write name into array
-# - get json array of gistnames
+# - get json array of gistnames, turn into bash array
 gist_names_json_array=$(curl -sL -X GET -H "Authorization: token $GITHUB_TOKEN"     https://api.github.com/gists | jq -s  'map(.[0], .[].files | keys) | flatten ')
-gist_names=$(echo $gist_names_json_array | jq -r '.[]')
-
-# get list of all files in subdirectory json_all
+gist_names=$(echo $gist_names_json_array | jq -r '.[]') 
+# if gist_names is empty, stop
+if [ ${#gist_names} -eq 0 ]; then
+    echo "List 'gist_names' from github.com is empty, cannot continue"
+    exit 1
+fi
 for file in json_all/*.txt; do
     # get filename without path
     filename=$(basename "$file")
-    # get file modification date of $file
-
+    # get file modification date of $file, make it part of "Description:"
     mdate=$(stat -c %y "$file" | cut -d' ' -f1)
     # get description from line containing "content:"
     description=$(head -1 "$file" | grep '"content":'  | sed 's/"content": //g' | sed 's/[#"]//g')
