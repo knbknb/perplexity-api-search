5e8e414b325bdd759a59a809bc23a622191d0018
Knut Behrends <knb@gfz-potsdam.de>
2024-02-02 21:13:20 +0100

    Refactor function placement

--- explore_perplexity_api.sh
+++ explore_perplexity_api.sh
@@ -9,6 +9,89 @@
 
 # example usage:
 # . ./explore_perplexity_api.sh --prompt "translate this into emojis: 'you and me'" --slug "emojis-you-me"
+
+####################### FUNCTIONS ############################
+
+extract_json_from_html_report() {
+    # not robust: xmllint --html --xpath "//code/text()" "$report_file"
+    # robust way to extract the JSON from the HTML reports: xidel
+    local report="$1"
+    local json_outfile="$2"
+    xidel --input "$report_file" --html -s -e  "//code/text()" \
+    | tail -n +3 \
+    | head -n -1 > "$json_outfile" 
+}
+
+# uses jq -rs to wrap JSON fragments (with commas missing) 
+# into an enclosing array, properly separated with commas
+enclose_filecontent_in_array() {
+    local json_file="$1"
+
+    cp "$json_file" "$json_file.json"
+    < "$json_file.json" jq -rs "." > "$json_file"
+    rm "$json_file.json"
+}
+
+write_json_output_to_stdout() {
+        local json_outfile="$1"
+
+        if [ ! -s "$json_outfile" ]; then
+            echo ""
+            echo "ERROR: $json_outfile is empty (?)"
+            echo ""
+        else 
+            < "$json_outfile" \
+            jq -r '["##### ", .[1].model, "\n\n", .[1].choices[0].message.content, "\n\n"] | join("")' \
+            | fmt
+        fi
+}
+
+# Function to generate shell pipeline code 
+# for pretty-printing formatted text encoded in from JSON files
+display_all_results() {
+        local slug="$1"
+        cat <<EOF
+# now run this to see the results, extracted from the json files
+< final_output/*$slug*.json jq -r ' .[]| ["###### ", .[0].model, .[].choices[0].message.content, "\n\n"] | join("        ")' \ 
+ | pandoc -f markdown -t html | lynx -stdin -dump > final_output/$slug.txt
+or
+fmt final_output/*$slug*.json  
+    
+EOF
+}
+
+# json to html to pretty printed text
+finalize_json_files() {
+    local slug="$1"
+    local outfile="final_output/$slug.json"
+
+    rm "$outfile" 2>/dev/null
+    ls -1 json_extracted/*"$slug"*.json | xargs -i bash -c "jq -rs < \"{}\"  >> $outfile"
+    enclose_filecontent_in_array "$outfile"
+    cp "$outfile" "$outfile.json"
+    # requires jq 1.7
+    < "$outfile.json" jq '[.[].[]]' > "$outfile"
+    rm "$outfile.json"
+    # add the question to the top of the file
+    question=$(< "$outfile" grep '"role": "user"' -A 1  | tail -1)
+    # Trim leading whitespace
+    question="${question#"${question%%[![:space:]]*}"}"
+    echo "# $question" > "final_output/$slug.txt"
+    
+    # ! ---> write answers to file, nicely human readable
+    < "$outfile" jq -r '.[]| ["<hr>## ", .[0].model, "<hr>\n\n", .[].choices[0].message.content, "\n\n"] | join(" ")' \
+    | pandoc -f markdown -t html | lynx -stdin -dump | fmt >> "final_output/$slug.txt"
+
+    # optional: add the question to the end of the file
+    echo "" >> "final_output/$slug.txt"
+    echo "---------------------" >> "final_output/$slug.txt"
+    echo "" >> "final_output/$slug.txt"
+    echo "$question" >> "final_output/$slug.txt"
+}
+
+
+####################### MAIN ############################
+
 SLUG="prompt-slug"
 PROMPT="your question or task"
 # manually override the default persona with a custom one
@@ -95,79 +178,36 @@ custom_instruction=$(jq <<EOF
 EOF
 )
 
-
-# uses jq -rs to wrap JSON fragments (with commas missing) 
-# into an enclosing array, properly separated with commas
-enclose_filecontent_in_array() {
-    local json_file="$1"
-
-    cp "$json_file" "$json_file.json"
-    < "$json_file.json" jq -rs "." > "$json_file"
-    rm "$json_file.json"
-}
-
-write_json_output_to_stdout() {
-        local json_outfile="$1"
-
-        if [ ! -s "$json_outfile" ]; then
-            echo ""
-            echo "ERROR: $json_outfile is empty (?)"
-            echo ""
-        else 
-            < "$json_outfile" \
-            jq -r '["##### ", .[1].model, "\n\n", .[1].choices[0].message.content, "\n\n"] | join("")' \
-            | fmt
-        fi
-}
-
-# Function to generate shell pipeline code 
-# for pretty-printing formatted text encoded in from JSON files
-display_all_results() {
-        local slug="$1"
-        cat <<EOF
-# now run this to see the results, extracted from the json files
-< final_output/*$slug*.json jq -r ' .[]| ["###### ", .[0].model, .[].choices[0].message.content, "\n\n"] | join("        ")' \ 
- | pandoc -f markdown -t html | lynx -stdin -dump > final_output/$slug.txt
-or
-fmt final_output/*$slug*.json  
-    
-EOF
-}
-
 for model in $models; do
     # use jq to replace the MODEL in the custom_instruction
     prompt_json=$(echo "$custom_instruction" | jq --arg MODEL "$model" '.model |= $MODEL')
     #echo "$prompt_json" 
-   
-    query_file="${query_dir}/${SLUG}${model}.json"
+    outfile_json="${SLUG}${model}.json"
+    outfile_html="${SLUG}${model}.html"
+    query_file="${query_dir}/$outfile_json"
+    report_file="${html_outdir}/$outfile_html"
     #echo "Writing $query_file"    
     <"$collection_file" jq --arg RAW "$prompt_json" --arg NAME "$model" '.item[0].request.body.raw |= $RAW | .name |= $NAME' > "$query_file"
       
-    #
-    #report="newman/$SLUG--$model---$(date +%Y-%m-%d-%H-%M-%S).html"
-    report="newman/$SLUG--$model.html"
 
     echo "$model: Saving into $query_file"
-    echo newman run "$query_file" -e "$modif_environment_file" \
+    newman run "$query_file" -e "$modif_environment_file" \
        -r htmlextra \
-       --reporter-htmlextra-export "$report" \
+       --reporter-htmlextra-export "$report_file" \
        --reporter-htmlextra-skipHeaders "Authorization" \
        --reporter-htmlextra-browserTitle "$model: $prompt" \
        --reporter-htmlextra-title "$model: $prompt"
-    continue
+    echo ""
 
     file_name=$(basename "$query_file")
     base_name="${file_name%.*}"
     json_outfile="$json_outdir/$base_name.json"
-    if [ ! -f "$report" ]; then
-        echo "Skipping $report"
+    if [ ! -f "$report_file" ]; then
+        echo "Skipping '$report_file', file unavailable"
         continue
     else 
-     # not robust: xmllint --html --xpath "//code/text()" "$report"
-     # robust way to extract the JSON from the HTML reports:
-      xidel --input "$report" --html -s -e  "//code/text()" \
-        | tail -n +3 \
-        | head -n -1 > "$json_outfile" 
+        # extract the JSON from the HTML reports
+        extract_json_from_html_report "$report_file" "$json_outfile"
     fi
 
 
@@ -179,35 +219,6 @@ for model in $models; do
     fi
 
 done
-
-# json to html to pretty printed text
-finalize_json_files() {
-    local slug="$1"
-    local outfile="final_output/$slug.json"
-
-    rm "$outfile" 2>/dev/null
-    ls -1 json_extracted/*"$slug"*.json | xargs -i bash -c "jq -rs < \"{}\"  >> $outfile"
-    enclose_filecontent_in_array "$outfile"
-    cp "$outfile" "$outfile.json"
-    # requires jq 1.7
-    < "$outfile.json" jq '[.[].[]]' > "$outfile"
-    rm "$outfile.json"
-    # add the question to the top of the file
-    question=$(< "$outfile" grep '"role": "user"' -A 1  | tail -1)
-    # Trim leading whitespace
-    question="${question#"${question%%[![:space:]]*}"}"
-    echo "# $question" > "final_output/$slug.txt"
-    
-    # ! ---> write answers to file, nicely human readable
-    < "$outfile" jq -r '.[]| ["<hr>## ", .[0].model, "<hr>\n\n", .[].choices[0].message.content, "\n\n"] | join(" ")' \
-    | pandoc -f markdown -t html | lynx -stdin -dump | fmt >> "final_output/$slug.txt"
-
-    # optional: add the question to the end of the file
-    echo "" >> "final_output/$slug.txt"
-    echo "---------------------" >> "final_output/$slug.txt"
-    echo "" >> "final_output/$slug.txt"
-    echo "$question" >> "final_output/$slug.txt"
-}
 
 finalize_json_files "$SLUG"
 ##display_all_results "$SLUG"
